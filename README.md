912a88a963c4705333f316f3008401d64a4ae46e
Somnath Sikdar <somnath.sikdar@metronom.com>
2020-11-18 17:26:47 +0100

    first commit; basic bayes approach

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a repository for code + notex for the course on Statistical Rethinking 
+by Richard McElreath. The lectures for 2019 can be found here:
+
+https://youtu.be/4WVelCswXo4
+
+The original code is in R, but here we have ported it to Python.
---
+++ code/lec02/grid_compute.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import binom
+
+NUM_PTS = 1000
+
+p_grid = np.linspace(start=0, stop=1, num=NUM_PTS)
+
+# The prior is a uniform distribution Uniform(0, 1)
+prior = [1] * NUM_PTS
+
+# The likelihood is Binomial(n, p)
+likelihood = binom.pmf(k=6, n=9, p=p_grid)
+
+# Compute the un-normalized posterior
+unnormal_posterior = likelihood * prior
+
+# Normalized posterior
+posterior = unnormal_posterior / sum(unnormal_posterior)
+
+plt.plot(p_grid, posterior)
+plt.xlabel('Probability of water')
+plt.ylabel('Posterior probability')
+plt.title(f'Sample= WLWWWLWLW; number of grid points = {NUM_PTS}')
+plt.show()
