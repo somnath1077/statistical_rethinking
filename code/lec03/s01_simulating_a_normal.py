5b0eef8fc50069323fafa547d71f5b21e4027ec5
Somnath Sikdar <somnath.sikdar@metronom.com>
2020-11-20 14:54:26 +0100

    started with lec 3. simulating a normal
    Signed-off-by: Somnath Sikdar <somnath.sikdar@metronom.com>

--- code/lec03/s01_simulating_a_normal.py
+++ code/lec03/s01_simulating_a_normal.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-from scipy.stats import binom
+from scipy.stats import binom, norm
 
 sns.set_style('whitegrid')
 
 NUM_PEOPLE = 1000
-NUM_TOSSES = 20
+NUM_TOSSES = 200
 
 dist_from_center_line = np.zeros(NUM_PEOPLE)
 
@@ -17,5 +17,25 @@ for i in range(NUM_TOSSES):
     steps_to_move = np.where(tosses == 0, -1, 1)
     dist_from_center_line += steps_to_move
 
+sample_mu = np.mean(dist_from_center_line)
+sample_sd = np.std(dist_from_center_line)
+
+min_dist = np.min(dist_from_center_line)
+max_dist = np.max(dist_from_center_line)
+
+d_range = np.linspace(min_dist, max_dist, NUM_PEOPLE)
+normal_data = norm.pdf(x=d_range, loc=sample_mu, scale=sample_sd)
+
 sns.kdeplot(dist_from_center_line)
+
+plt.xlabel('Distance from center line')
+plt.ylabel('Number of people')
+plt.title(f'Number of people = {NUM_PEOPLE}, number of tosses = {NUM_TOSSES}')
+
+plt.axvline(x=sample_mu, linestyle='--', color='b', label='mean')
+plt.axvline(x=sample_mu + sample_sd, linestyle='--', color='r', label='mean + sd')
+plt.axvline(x=sample_mu - sample_sd, linestyle='--', color='g', label='mean - sd')
+
+plt.plot(d_range, normal_data, linestyle='--', color='black')
+plt.legend()
 plt.show()
