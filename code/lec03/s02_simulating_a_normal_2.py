6ce38b6100a5427a2bfd7c2f0c42a66b22420212
Somnath Sikdar <somnath.sikdar@metronom.com>
2020-11-20 16:43:53 +0100

    normal simulation 2
    Signed-off-by: Somnath Sikdar <somnath.sikdar@metronom.com>

--- code/lec03/s02_simulating_a_normal_2.py
+++ code/lec03/s02_simulating_a_normal_2.py
@@ -12,6 +12,12 @@ NUM_SAMPLES = 12
 growth_rates = [np.prod(uniform.rvs(loc=1.0, scale=0.1, size=NUM_SAMPLES))
                 for _ in range(REPETITIONS)]
 
+big = [np.prod(uniform.rvs(loc=1.0, scale=0.5, size=NUM_SAMPLES))
+       for _ in range(REPETITIONS)]
+
+small = [np.prod(uniform.rvs(loc=1.0, scale=0.01, size=NUM_SAMPLES))
+         for _ in range(REPETITIONS)]
+
 
 def dist_plot(vals, norm_compare=False):
     sns.distplot(vals, label='Distribution plot')
@@ -30,3 +36,5 @@ def dist_plot(vals, norm_compare=False):
 
 
 dist_plot(growth_rates, norm_compare=True)
+dist_plot(big, norm_compare=True)
+dist_plot(small, norm_compare=True)
